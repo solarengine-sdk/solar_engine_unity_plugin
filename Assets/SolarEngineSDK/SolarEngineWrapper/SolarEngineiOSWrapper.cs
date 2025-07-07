1dde181473030ee4e17ffd9a62354ca9e7576175
iMengqing <mengqing.gao@mintegral.com>
2025-07-07 15:53:07 +0800

    修复iOSwrapper

--- Assets/SolarEngineSDK/SolarEngineWrapper/SolarEngineiOSWrapper.cs
+++ Assets/SolarEngineSDK/SolarEngineWrapper/SolarEngineiOSWrapper.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 namespace SolarEngine
 {
     public partial class Analytics : MonoBehaviour
-    {e
+    {
         private static Dictionary<string, object> GetPresetProperties()
         {
 
